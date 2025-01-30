1fe429c9b1418822acb6aff2c962598b6d5a0e6e
dksijide <jehudizon40@gmail.com>
2025-01-30 12:04:32 +0800

    Update and rename kids.lua to kidss.lua

--- kidss.lua
+++ kidss.lua
@@ -1,4 +1,4 @@
-local allowedHWIDs = {
+{
     "CC341C50-CCFC-4E28-864C-425F627D86F2",  -- First allowed HWID (example)
     "A57B4E88-9C99-44B7-9899-5C94DB4B2C4A",  -- Second allowed HWID (example)
     -- Add more HWIDs as needed
