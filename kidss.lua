b108fc3055c09ebdb8821533f34d2703531b4518
dksijide <jehudizon40@gmail.com>
2025-01-30 14:19:22 +0800

    Update kidss.lua

--- kidss.lua
+++ kidss.lua
@@ -1,9 +1,13 @@
--- External script content (kids.lua)
+local playerHWID = game:GetService("RbxAnalyticsService"):GetClientId()
 
--- allowedHWIDs is passed as an argument
-local allowedHWIDs = {
-    "CC341C50-CCFC-4E28-864C-425F627D86F2",  -- First allowed HWID
-    "A57B4E88-9C99-44B7-9899-5C94DB4B2C4A",  -- Second allowed HWID
-}
+local isAllowed = false
+for _, hwid in pairs(allowedHWIDs) do
+    if playerHWID == hwid then
+        isAllowed = true
+        break
+    end
+end
 
--- The allowedHWIDs list is now available for use in the main script
+if not isAllowed then
+    return
+end
